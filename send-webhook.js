4311402ebb221e9f9cfbd730ee01071c2f6349d2
Mid <125941391+MidnightTale@users.noreply.github.com>
2023-07-13 05:39:51 -0700

    Update send-webhook.js

--- send-webhook.js
+++ send-webhook.js
@@ -1,36 +1,59 @@
 const axios = require('axios');
 
-const webhookUrl = process.env.WEBHOOK_URL;
+const discordWebhookURL = process.env.DISCORD_WEBHOOK_URL;
+const commitURL = process.env.GITHUB_SERVER_URL + '/' + process.env.GITHUB_REPOSITORY + '/commit/' + process.env.GITHUB_SHA;
 
-const commitMessage = process.env.GITHUB_EVENT_NAME === 'pull_request' ?
-  process.env.GITHUB_EVENT_PULL_REQUEST_TITLE :
-  process.env.GITHUB_EVENT_COMMIT_MESSAGE;
+const generateChangesList = (changes) => {
+  if (changes.length === 0) {
+    return 'None';
+  }
 
-const commitAuthor = process.env.GITHUB_EVENT_NAME === 'pull_request' ?
-  process.env.GITHUB_EVENT_PULL_REQUEST_USER_LOGIN :
-  process.env.GITHUB_EVENT_AUTHOR;
+  return changes.map((change) => `:white_check_mark: ${change}`).join('\n');
+};
 
-const embedData = {
-  title: 'IFC BAN LIST',
-  description: `**${commitAuthor}** edit`,
-  fields: [
+const payload = {
+  content: 'New commit',
+  embeds: [
     {
-      name: 'Commit',
-      value: commitMessage || 'No commit.',
+      title: 'Commit Details',
+      url: commitURL,
+      description: 'New commit has been made to the repository.',
+      color: 16777215, // White color
+      fields: [
+        {
+          name: ':pencil2: Commit Message',
+          value: process.env.GITHUB_COMMIT_MESSAGE,
+        },
+        {
+          name: ':bust_in_silhouette: Author',
+          value: process.env.GITHUB_ACTOR,
+          inline: true,
+        },
+        {
+          name: ':heavy_plus_sign: Added Files',
+          value: generateChangesList(process.env.GITHUB_ADDED_FILES.split('\n')),
+          inline: true,
+        },
+        {
+          name: ':pencil: Modified Files',
+          value: generateChangesList(process.env.GITHUB_MODIFIED_FILES.split('\n')),
+          inline: true,
+        },
+        {
+          name: ':wastebasket: Deleted Files',
+          value: generateChangesList(process.env.GITHUB_DELETED_FILES.split('\n')),
+          inline: true,
+        },
+      ],
     },
   ],
-  color: 16711680, // Red color
 };
 
-const requestData = {
-  embeds: [embedData],
-};
-
-axios.post(webhookUrl, requestData)
-  .then(response => {
-    console.log('Webhook sent successfully');
+axios
+  .post(discordWebhookURL, payload)
+  .then(() => {
+    console.log('Discord webhook notification sent successfully.');
   })
-  .catch(error => {
-    console.error('Error sending webhook', error);
-    process.exit(1); // Terminate the script with a non-zero exit code on error
+  .catch((error) => {
+    console.error('Error sending Discord webhook notification:', error);
   });
