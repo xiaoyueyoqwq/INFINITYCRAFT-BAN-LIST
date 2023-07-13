2af9572d4cfb1a841840e4c80ee4011d10dca19d
Mid <125941391+MidnightTale@users.noreply.github.com>
2023-07-13 05:24:31 -0700

    Create send-webhook.js

---
+++ send-webhook.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+const webhookUrl = process.env.WEBHOOK_URL;
+
+const commitMessage = process.env.GITHUB_EVENT_NAME === 'pull_request' ?
+  process.env.GITHUB_EVENT_PULL_REQUEST_TITLE :
+  process.env.GITHUB_EVENT_COMMIT_MESSAGE;
+
+const commitAuthor = process.env.GITHUB_EVENT_NAME === 'pull_request' ?
+  process.env.GITHUB_EVENT_PULL_REQUEST_USER_LOGIN :
+  process.env.GITHUB_EVENT_AUTHOR;
+
+const embedData = {
+  title: 'IFC BAN LIST',
+  description: `**${commitAuthor}** edit`,
+  fields: [
+    {
+      name: 'Commit',
+      value: commitMessage,
+    },
+  ],
+  color: 16711680, // Red color
+};
+
+const requestData = {
+  embeds: [embedData],
+};
+
+axios.post(webhookUrl, requestData)
+  .then(response => {
+    console.log('Webhook sent successfully');
+  })
+  .catch(error => {
+    console.error('Error sending webhook', error);
+  });
